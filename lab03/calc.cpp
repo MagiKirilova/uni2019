3c7d7c67cc0a74290a78a6f50b1b8f8e01d2ea9e
MagiKirilova <kirilova.margarita10@gmail.com>
2019-10-17 09:03:36 +0300

    Calc app added on 17.10

---
+++ lab03/calc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float x, y;
+ 	cin >> x;
+	cin >> y;
+
+	cout << x << " + " << y << " = " << x+y << endl;
+
+	cout << x << " - " << y << " = " << x-y << endl;
+	cout << x << " * " << y << " = " << x*y << endl;
+	cout << x << " / " << y << " = " << x/y << endl; 
+	
+
+	return 0;
+
+} 
